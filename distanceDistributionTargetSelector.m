06a46535628ef1c767d335a8785499f894ce8f26
havebeen <havebeen@gapp.nthu.edu.tw>
2022-10-27 15:35:00 +0800

    fixxing mouse click shift

--- distanceDistributionTargetSelector.m
+++ distanceDistributionTargetSelector.m
@@ -1,5 +1,6 @@
 function distanceDistributionTarget = distanceDistributionTargetSelector(app)
     distanceDistributionTargetProperties_temp = drawpoint(app.AxesForPrTargeting, 'Visible', 'off');
-    distanceDistributionTarget = distanceDistributionTargetProperties_temp.Position(1);
-    app.TargetDistanceEditField.Value = distanceDistributionTargetProperties_temp.Position(1);
+    
+    distanceDistributionTarget = distanceDistributionTargetProperties_temp.Position(1)-0.03;
+    app.TargetDistanceEditField.Value = distanceDistributionTargetProperties_temp.Position(1)-0.03;
 end

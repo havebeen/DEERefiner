3537da47e2b7b4245656c5db04a3a3a8817198f4
havebeen <havebeen@gapp.nthu.edu.tw>
2022-12-26 16:16:13 +0800

    adding RMSE maximum

--- main/RMSEAutoDeterminator.m
+++ main/RMSEAutoDeterminator.m
@@ -1,6 +1,9 @@
 function RMSEAutoDeterminator(app)
     distanceDistributionNumber = length(app.DEERRefineRestraintsTable.Data(:, 1));
     app.RMSE = distanceDistributionNumber*0.05+1.1;
+    if app.RMSE>=3
+        app.RMSE=3;
+    end
     app.RMSEEditField.Value = app.RMSE;
     app.RMSEEditField.FontColor = [0 0 0];
 end

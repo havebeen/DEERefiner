856231939cdb980982c8f5b83c5b49204407ea11
havebeen <havebeen@gapp.nthu.edu.tw>
2023-01-08 17:53:20 +0800

    updating OF example final PDB generator

--- Example/LmrP_OutwardFacing/WorkingFolder_2022_12_30_14_57_21_LmrP_OF/DEERefineFinalFileCopier.m
+++ Example/LmrP_OutwardFacing/WorkingFolder_2022_12_30_14_57_21_LmrP_OF/DEERefineFinalFileCopier.m
@@ -1,8 +1,10 @@
 function DEERefineFinalFileCopier(timeStamp)
     DEERefineFinalDistanceDistributionFileName = strcat("FINALPr_",  timeStamp, ".mat");
+    DEERefineFinalDistanceDistributionFigureFileName = strcat("FINALPr_",  timeStamp, ".fig");
     FINALPDBENSEMBLEFileName = strcat("FINALPDBENSEMBLE_",  timeStamp, ".pdb");
     FINALPDBFileName = strcat("FINALPDB_",  timeStamp, ".pdb");
     copyfile(DEERefineFinalDistanceDistributionFileName, '..')
     copyfile(FINALPDBENSEMBLEFileName, '..')
     copyfile(FINALPDBFileName, '..')
+    copyfile(DEERefineFinalDistanceDistributionFigureFileName, '..')
 end
--- Example/LmrP_OutwardFacing/WorkingFolder_2022_12_30_14_57_21_LmrP_OF/DEERefineFinalFileGenerator.m
+++ Example/LmrP_OutwardFacing/WorkingFolder_2022_12_30_14_57_21_LmrP_OF/DEERefineFinalFileGenerator.m
@@ -10,6 +10,27 @@ function DEERefineFinalFileGenerator(residue1List, residue2List, FINALPDBENSEMBL
 
     DEERefineFinalDistanceDistributionFileName = strcat("FINALPr_",  timeStamp, ".mat");
     DEERefineFinalDistanceDistribution = struct('targetDistanceDistributionFinal', normalizedTargetDistanceDistributionFinal,...
-                                                'DistanceDistributionFinal', DistanceDistributionFinal);
+                                                'DistanceDistributionFinal', DistanceDistributionFinal, ...
+                                                'residueIndex1', residue1List, ...
+                                                'residueIndex2', residue2List);
     save(DEERefineFinalDistanceDistributionFileName, '-struct', 'DEERefineFinalDistanceDistribution');
+    
+    DEERefinerFigure = figure;
+    figureHeight = 5;
+    figureWiedth = ceil(distanceDistributionNumber/5);
+    for distanceDistributionIndex = 1:distanceDistributionNumber
+        subplot(figureHeight, figureWiedth, distanceDistributionIndex)
+        hold on
+        plot(normalizedTargetDistanceDistributionFinal(:, 1, distanceDistributionIndex)/10, normalizedTargetDistanceDistributionFinal(:, 2, distanceDistributionIndex), 'k', 'linewidth', 2.5);
+        plot(DistanceDistributionFinal(:, 1, distanceDistributionIndex)/10, DistanceDistributionFinal(:, 2, distanceDistributionIndex), 'r', 'linewidth', 1.8);
+        xlim([1 9])
+        title(strcat(num2str(residue1List(distanceDistributionIndex)), '/', num2str(residue2List(distanceDistributionIndex)), 'R1'))
+        xlabel('Distance, nm')
+        box on
+        set(gca,'FontSize',16)
+        yticks([])
+        ylabel('P(r)')
+    end
+    saveas(DEERefinerFigure, strcat("FINALPr_",  timeStamp, ".fig"))
+    close(DEERefinerFigure)
 end

bbbfda93066c1a2fec36f42d83f38c4bfe3d4522
havebeen <havebeen@gapp.nthu.edu.tw>
2023-01-08 17:48:54 +0800

    final PDB generator bug fixing

--- main/minimalRMSEPDBGenerator.m
+++ main/minimalRMSEPDBGenerator.m
@@ -1,5 +1,5 @@
 function minimalRMSEIndex = minimalRMSEPDBGenerator(FINALPDBENSEMBLE, FINALPDBMaximalDifference, minimalFINALPDBMaximalDifference, FINALPDBRMSE, minimalFINALPDBRMSE, timeStamp)
     FINALPDBFileName = strcat("FINALPDB_",  timeStamp, ".pdb");
     minimalRMSEIndex = find((FINALPDBMaximalDifference == minimalFINALPDBMaximalDifference &FINALPDBRMSE== minimalFINALPDBRMSE)==1);
-    pdbSaver(FINALPDBFileName, FINALPDBENSEMBLE(:, :, minimalRMSEIndex));
+    pdbSaver(FINALPDBFileName, FINALPDBENSEMBLE(:, :, minimalRMSEIndex(1)));
 end
